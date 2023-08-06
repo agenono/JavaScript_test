d480e354ea2004bbf7033540a911ac372c59ca45
Chisato Ageno <mikanageno@gmail.com>
2023-08-06 22:14:34 +0900

    no message

--- 03-demo3/js/script.js
+++ 03-demo3/js/script.js
@@ -1,5 +1,2 @@
 console.log(document.querySelector("#colorPicker").value);
-//1行コメントアウト
-/*複数行のコメントアウト
-この範囲は動作に影響しません*/
-document.querySelector("#colorText").textContent = "あいうえお";
+document.querySelector("#colorText").textContent = "カラーコード：";
